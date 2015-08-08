97fcea6af20a2b9171e73d175b11396c77c72991
Hosain <contact@hosain.net>
2015-08-08 22:21:21 +0100

    Implemented basic functionality of the generate button
    Added two examples lib rules

--- js/proguardrules.js
+++ js/proguardrules.js
@@ -5,4 +5,18 @@ $(function () {
             minimumInputLength: 1
         }
     );
+    $('#generate-button').click(function () {
+        var libs = $('#search-box').val();
+        if (libs) {
+            $('#rules').empty();
+            $('#rules-container').show();
+            libs.forEach(function (element, index, array) {
+                $.get("rules/" + element + ".txt", function (data) {
+                    $('#rules').append(data.replace(/\n/g, "<br/>"));
+                    $('#rules').append("<br/><br/>")
+                    console.log(data);
+                });
+            });
+        }
+    })
 });
